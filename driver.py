ee4fdf8c149b316b605348a28c134a6f3aa0ec72
calvincodes <arpitjain1821@gmail.com>
2019-02-26 21:25:37 -0600

    Adding link for raw_dataset.csv

--- driver.py
+++ driver.py
@@ -3,6 +3,9 @@ import csv
 
 csv.field_size_limit(sys.maxsize)
 
+# raw_dataset.csv can be found here.
+# Please download and keep the file in main directory while running this script.
+# https://drive.google.com/open?id=1TnO5pwOMPFUdjdqHw2fXba0UVV0B0y4i
 with open('raw_dataset.csv') as sample_csv:
     csv_reader = csv.reader(sample_csv)
     line_count = 0
